46f1f60154a226c476a3deb497b8098068bd295c
keerthivasan123 <keerthivasan287@gmail.com>
2019-05-26 22:06:00 +0530

    deleted all unessary files

--- directory.js
+++
@@ -1,9 +0,0 @@
-
-var fs = require('fs');
-
-var file = fs.readFile('readMe.txt','utf8',function(err,data){
-    fs.writeFile('writeMe.txt',data,(error) => { /* handle error */ });
-    console.log(data);
-});
-
-console.log('This is an instruction outside the sync file system.');
