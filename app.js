20705e9e6c34d6523eb32300aeb0aa73847a951a
keerthivasan123 <keerthivasan287@gmail.com>
2019-05-26 22:53:28 +0530

    app.js in now bug free

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 var express = require('express');
+var todoControler = require('./controllers/todoController');
 var app = express();
 
 app.set('view engine','ejs');
-app.use('/asserts',express.static('asserts'));
+app.use(express.static(__dirname + '/asserts'));
 
-app.get('/',function(req,res){
-    res.send('This is Home Page');
-});
-
-app.get('/contact',function(req,res){
-    res.send('This is contact Page');
-});
-
-app.get('/profile/:name',function(req,res){
-    res.render('profile',{person : req.params.name});
-});
+todoControler(app);
 
 
 app.listen(3000);
